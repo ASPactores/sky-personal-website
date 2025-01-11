c304a272cc79ef143b3d06d72f3ba80323f79938
ASPactores <anakin.pactores@gmail.com>
2025-01-12 00:33:30 +0800

    feat: add AboutMe component and integrate it into the homepage

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,7 +1,8 @@
-import ParticleBackground from "@/components/ParticleBackground";
-import BackgroundGradient from "@/components/BackgroundGradient";
-import NavigationBar from "@/components/NavigationBar";
-import Hero from "@/components/Hero";
+import ParticleBackground from '@/components/ParticleBackground'
+import BackgroundGradient from '@/components/BackgroundGradient'
+import NavigationBar from '@/components/NavigationBar'
+import Hero from '@/components/Hero'
+import AboutMe from '@/components/AboutMe'
 
 export default function Home() {
   return (
@@ -9,16 +10,17 @@ export default function Home() {
       <Background />
       <NavigationBar />
       <Hero />
+      <AboutMe />
       <ParticleBackground />
     </div>
-  );
+  )
 }
 
 function Background() {
   return (
     <div className="absolute w-full h-full overflow-clip -z-10">
-      <BackgroundGradient className="absolute -right-96 -top-4"/>
-      <BackgroundGradient className="absolute -left-96 top-[80vh]"/>
+      <BackgroundGradient className="absolute -right-96 -top-4" />
+      <BackgroundGradient className="absolute -left-96 top-[80vh]" />
     </div>
   )
 }
---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+
+export default function AboutMe() {
+  return (
+    <div className="flex items-center justify-center h-screen w-full">
+      <div className="flex flex-col items-center justify-center text-primary-50 text-center my-4 w-[50vw]">
+        <p className="text-4xl font-semibold text-left w-full mb-6">About Me</p>
+        <div className="grid grid-cols-3 gap-9 items-center w-[90%]">
+          <p className="text-left col-span-2 text-sm">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+            has been the industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum
+            is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a
+            galley of type and scrambled it to make a type specimen book.
+          </p>
+          <div className="relative w-[250px] h-[250px] group hover:drop-shadow-primary-active transition-all duration-300">
+            <div className="absolute inset-0 bg-accent-900 opacity-50 rounded-8 z-10 group-hover:opacity-0 transition-opacity duration-300 hover:cursor-pointer"></div>
+            <Image
+              src="/sky-profile.jpg"
+              alt="Profile Photo"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-8"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}

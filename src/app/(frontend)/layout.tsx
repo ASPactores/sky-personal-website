be22079a40a0d89ba42c113adf7fbcd05118ac1d
ASPactores <anakin.pactores@gmail.com>
2025-02-18 18:50:06 +0800

    feat: add @vercel/speed-insights package and integrate it into the layout

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import './globals.css'
 
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className="bg-primary-950">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )

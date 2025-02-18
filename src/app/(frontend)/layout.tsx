45ffc9bae33cb2d39fac8a44ef1ae44732e97ef7
ASPactores <anakin.pactores@gmail.com>
2025-02-18 18:48:04 +0800

    feat: add @vercel/analytics package and integrate analytics component into layout

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,23 +1,24 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
+
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Anakin Skywalker Pactores",
-  description: "Personal Portfolio of Anakin Skywalker Pactores",
-};
+  title: 'Anakin Skywalker Pactores',
+  description: 'Personal Portfolio of Anakin Skywalker Pactores',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className='bg-primary-950'
-      >
+      <body className="bg-primary-950">
         {children}
+        <Analytics />
       </body>
     </html>
-  );
+  )
 }
